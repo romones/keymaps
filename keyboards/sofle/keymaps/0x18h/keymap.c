d3ddab37af44a41daf904aee96d225cb3200a19e
Roman Florea <romones@Air.local>
2022-02-13 12:36:01 +0200

    Sofle RGB keymap.

---
+++ keyboards/sofle/keymaps/0x18h/keymap.c
@@ -0,0 +1,276 @@
+
+ /* Copyright 2021 Dane Evans
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+  // SOFLE RGB
+#include <stdio.h>
+
+#include QMK_KEYBOARD_H
+
+#define INDICATOR_BRIGHTNESS 30
+
+#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
+#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
+
+// Light combinations
+#define SET_INDICATORS(hsv) \
+	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {35+0, 1, hsv}
+#define SET_UNDERGLOW(hsv) \
+	{1, 6, hsv}, \
+    {35+1, 6,hsv}
+#define SET_NUMPAD(hsv)     \
+	{35+15, 5, hsv},\
+	  {35+22, 3, hsv},\
+	  {35+27, 3, hsv}
+#define SET_NUMROW(hsv) \
+	{10, 2, hsv}, \
+		{20, 2, hsv}, \
+		{30, 2, hsv}, \
+	  {35+ 10, 2, hsv}, \
+	  {35+ 20, 2, hsv}, \
+	  {35+ 30, 2, hsv}
+#define SET_INNER_COL(hsv)	\
+	{33, 4, hsv}, \
+	  {35+ 33, 4, hsv}
+
+#define SET_OUTER_COL(hsv) \
+	{7, 4, hsv}, \
+	  {35+ 7, 4, hsv}
+#define SET_THUMB_CLUSTER(hsv) 	\
+	{25, 2, hsv}, \
+	  {35+ 25, 2, hsv}
+#define SET_LAYER_ID(hsv) 	\
+	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+		{1, 6, hsv}, \
+    {35+1, 6, hsv}, \
+		{7, 4, hsv}, \
+	  {35+ 7, 4, hsv}, \
+		{25, 2, hsv}, \
+	  {35+ 25, 2, hsv}
+
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_O LALT_T(KC_O)
+#define HOME_E LGUI_T(KC_E)
+#define HOME_U LSFT_T(KC_U)
+
+// Right-hand home row mods
+#define HOME_H RSFT_T(KC_H)
+#define HOME_T RGUI_T(KC_T)
+#define HOME_N LALT_T(KC_N)
+#define HOME_S RCTL_T(KC_S)
+
+enum sofle_layers {
+    _DEFAULTS = 0,
+    _DVORAK = 0,
+    _MISSION,
+};
+
+enum custom_keycodes {
+    KC_QWERTY = SAFE_RANGE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_DVORAK] = LAYOUT(
+  //,------------------------------------------------.                    ,---------------------------------------------------.
+  KC_TRNS,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_GRV,
+  //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+  KC_TAB,  KC_QUOT,KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,   KC_C,    KC_R,    KC_L, KC_SLSH,
+  //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
+  KC_BSPC,  HOME_A, HOME_O,  HOME_E,  HOME_U,  KC_I,                      KC_D,    HOME_H, HOME_T,  HOME_N,  HOME_S,  KC_MINS,
+  //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
+  KC_TRNS,  KC_SCLN,KC_Q,    KC_J,    KC_K,    KC_X,  KC_TRNS,    KC_B,KC_B,    KC_M,   KC_W,    KC_V,    KC_Z,    KC_EQL,
+  //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
+                 KC_TRNS, KC_TRNS, KC_TRNS,  KC_SPC,  KC_TRNS,    KC_TRNS, KC_ENT,  KC_TRNS, KC_TRNS, KC_BSLS
+  //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
+),
+[_MISSION] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_1),
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_2),
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_3),
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_4),
+                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+};
+
+#ifdef RGBLIGHT_ENABLE
+char layer_state_str[70];
+// Now define the array of layers. Later layers take precedence
+
+// QWERTY,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_RED)
+
+);
+const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_PINK)
+);
+
+// _NUM,
+// Light on outer column and underglow
+const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_TEAL)
+
+);
+// _SYMBOL,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_BLUE)
+
+    );
+// _COMMAND,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_PURPLE)
+);
+
+//_NUMPAD
+const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_INDICATORS(HSV_ORANGE),
+    SET_UNDERGLOW(HSV_ORANGE),
+	SET_NUMPAD(HSV_BLUE),
+    {7, 4, HSV_ORANGE},
+    {25, 2, HSV_ORANGE},
+    {35+6, 4, HSV_ORANGE},
+    {35+25, 2, HSV_ORANGE}
+    );
+// _SWITCHER   // light up top row
+const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_GREEN),
+	SET_NUMROW(HSV_GREEN)
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+
+    layer_qwerty_lights,
+	layer_num_lights,// overrides layer 1
+	layer_symbol_lights,
+    layer_command_lights,
+	layer_numpad_lights,
+	layer_switcher_lights,  // Overrides other layers
+	layer_colemakdh_lights
+);
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_DVORAK));
+    return state;
+}
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+
+	rgblight_mode(10);// haven't found a way to set this in a more useful way
+
+}
+#endif
+
+#ifdef OLED_ENABLE
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+static void print_status_narrow(void) {
+    // Print current mode
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_ln_P(PSTR("0x18h"), false);
+
+    oled_write_ln_P(PSTR(""), false);
+
+	//snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state)
+
+
+    switch (get_highest_layer(default_layer_state)) {
+        case _DVORAK:
+            oled_write_ln_P(PSTR("Dvrk"), false);
+            break;
+
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+    }
+    oled_write_P(PSTR("\n\n"), false);
+    // Print current layer
+    oled_write_ln_P(PSTR("LAYER"), false);
+    switch (get_highest_layer(layer_state)) {
+        case _DVORAK:
+            oled_write_P(PSTR("Base\n"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+    }
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_logo();
+    }
+    return false;
+}
+
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_DVORAK);
+            }
+            return false;
+    }
+    return true;
+}
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+		} else if (index == 1) {
+			switch (get_highest_layer(layer_state)) {
+				case _DVORAK:
+					if (clockwise) {
+						tap_code(KC_PGDOWN);
+					} else {
+						tap_code(KC_PGUP);
+					}
+				break;
+		}
+    }
+    return true;
+}
+
+#endif
